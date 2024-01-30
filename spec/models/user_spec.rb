42683ddb452baf11a155672d8f75e0d4e696807b
Bruno Silva <brunosilvabrait@gmail.com>
2024-01-30 16:28:40 -0300

    Configuring tests with rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Espero validar os campos obrigatorios' do
+    it 'Validar o campo nome está presente' do
+      user = build(:user)
+
+      expect(user.valid?).to eq(true)
+      #expect(user).to be_truthy
+    end
+
+    it 'Validar o campo nome como nil' do
+      user = build(:user, name: nil)
+
+      expect(user.valid?).to eq(false)
+      #expect(user).to be_falsey
+    end
+
+    it 'Validar o campo telefone como nil' do
+      user = build(:user, phone: nil)
+
+      expect(user.valid?).to eq(false)
+    end
+
+    it 'Validar o campo email como nil' do
+      user = build(:user, email: nil)
+
+      expect(user.valid?).to eq(false)                      
+    end
+
+    it 'Validar o campo senha como nil' do
+      user = build(:user, password: nil)
+
+      expect(user.valid?).to eq(false)                      
+    end
+
+    context 'Valida email' do
+      it 'Validar emails únicos' do
+        user1 = create(:user) #user email: user1@gmail.com
+        user2 = build(:user, email: user1.email) # user1@gmail.com
+  
+        expect(user2.valid?).to eq(false)
+      end
+
+      it 'Validar mensagem de erro para emails únicos' do
+        user1 = create(:user)
+        user2 = build(:user, email: user1.email)
+
+        user2.save
+
+        expect(user2.errors.present?).to eq(true)
+        #expect(user2.erros.size).to be_positive
+      end
+    end
+
+  end
 end
