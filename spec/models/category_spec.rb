324185ea5cf81c4c169883bbcf022974b739f0d9
Bruno Silva <brunosilvabrait@gmail.com>
2024-02-02 14:16:31 -0300

    Rspec category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Espero validar os campos obrigatorios' do
+    it 'Validar os campos presentes' do
+      category = build(:category)
+
+      expect(category.valid?).to eq(true)
+    end
+
+    it 'Validar o campo nome como nil' do
+      category = build(:category, name: nil)
+
+      expect(category.valid?).to eq(false)
+    end
+
+    it 'Validar o campo posição como nil' do
+      category = build(:category, position: nil)
+
+      expect(category.valid?).to eq(false)
+    end
+  end
 end
