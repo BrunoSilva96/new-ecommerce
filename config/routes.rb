66236b3f71b067c2636455300a19e51399e24e72
Bruno Silva <brunosilvabrait@gmail.com>
2024-02-15 21:06:55 -0300

    Added user password change

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'home#index'
-  
+
   resources :settings, only: [:index] do
     collection do
+      get :password
       patch :update_user
+      patch :update_password
     end
   end
-  
+
   namespace :admin do
     root to: 'home#index'
     resources :categories
